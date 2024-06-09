15f447951f0bd343249b6451ad108fd02815d52a
christo <christopher.pkm@gmail.com>
2024-06-10 00:19:33 +0100

    fixed bug where if you hit stopTimer()/call it before ever calling startTimer() it will return wrong time when subsequently then getting the time elapsed after calling startTimer()

--- StopWatch.test.js
+++ StopWatch.test.js
@@ -1,79 +1,95 @@
-
 // hello world test
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  expect(1 + 2).toBe(3);
 });
 
 const StopWatch = require("./dist/StopWatch");
 const sleep = require("./sleep");
 
-test('on object initialisation time elapsed in ms(millaseconds) to be equal to 0', () => {
-    const sw = new StopWatch();
-    expect(sw.getTimeElapsedInMs).toBe(0);
+test("on object initialisation time elapsed in ms(millaseconds) to be equal to 0", () => {
+  const sw = new StopWatch();
+  expect(sw.getTimeElapsedInMs).toBe(0);
 });
 
-test('on object initialisation before starting timer, stop watch format string to be \
-equal to 00:00:00 and to be same after calling reset()', () => {
-    const sw = new StopWatch();
-    expect(sw.getTimeElapsedInStopWatchFormatString).toBe("00:00:00");
-    sw.reset();
-    expect(sw.getTimeElapsedInStopWatchFormatString).toBe("00:00:00");
+test("on object initialisation when calling .stopTimer() before .startTimer(), subsequent checks/gets of time elapsed after then \
+calling .startTimer() work as expected", () => {
+  const sw = new StopWatch();
+  sw.stopTimer();
+  sw.startTimer();
+  expect(sw.getTimeElapsedInMs).toBeLessThanOrEqual(1000);
 });
 
-test('on timer start, after 1 second elapsed result to be equal or greater than 1000ms', done => {
-    const tollerenceInMs = 10;
-    const oneSecondInMs = 1000;
-    const sw = new StopWatch();
-    sw.startTimer();
-    setTimeout(() => {
-        sw.stopTimer();
-        testTimerDisplaysCorrectTimeAfterMsElapsed(sw, oneSecondInMs, tollerenceInMs);
-        done();
-    },oneSecondInMs);
+test("on object initialisation before starting timer, stop watch format string to be \
+equal to 00:00:00 and to be same after calling reset()", () => {
+  const sw = new StopWatch();
+  expect(sw.getTimeElapsedInStopWatchFormatString).toBe("00:00:00");
+  sw.reset();
+  expect(sw.getTimeElapsedInStopWatchFormatString).toBe("00:00:00");
 });
 
-test('start, stop, resume, reset, start: stopwatch timer', async () => {
-    jest.setTimeout(15000);
-
-    // will need to use await sleep()'s
-    const tollerenceInMs = 10;
-    const sw = new StopWatch();
-    sw.startTimer();
-    await sleep(970);
+test("on timer start, after 1 second elapsed result to be equal or greater than 1000ms", (done) => {
+  const tollerenceInMs = 10;
+  const oneSecondInMs = 1000;
+  const sw = new StopWatch();
+  sw.startTimer();
+  setTimeout(() => {
     sw.stopTimer();
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 970, tollerenceInMs);
-
-    sw.startTimer();
-    await sleep(30);
-    sw.stopTimer();
-    
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
-
-    await sleep(1000);
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
-    sw.startTimer();
-    await sleep(1000);
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 2000, tollerenceInMs);
-    await sleep(2000);
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 4000, tollerenceInMs);
-    sw.stopTimer();
-    await sleep(1000);
-    sw.stopTimer();
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 4000, tollerenceInMs);
-    sw.startTimer();
-    await sleep(1000);
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 5000, tollerenceInMs);
-    sw.reset();
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 0, tollerenceInMs);
-    sw.startTimer();
-    await sleep(1000);
-    testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
+    testTimerDisplaysCorrectTimeAfterMsElapsed(
+      sw,
+      oneSecondInMs,
+      tollerenceInMs
+    );
+    done();
+  }, oneSecondInMs);
 });
 
-function testTimerDisplaysCorrectTimeAfterMsElapsed(stopWatchObject, ms, msTollerence){
-    const re = new RegExp(`00:0${Math.floor(ms / 1000)}:0[0-9]`);
-    expect(stopWatchObject.getTimeElapsedInMs).toBeGreaterThanOrEqual(ms);
-    expect(stopWatchObject.getTimeElapsedInMs).toBeLessThanOrEqual(ms + msTollerence);
-    expect(stopWatchObject.getTimeElapsedInStopWatchFormatString).toMatch(re);
+test("start, stop, resume, reset, start: stopwatch timer", async () => {
+  jest.setTimeout(15000);
+
+  // will need to use await sleep()'s
+  const tollerenceInMs = 15;
+  const sw = new StopWatch();
+  sw.startTimer();
+  await sleep(970);
+  sw.stopTimer();
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 970, tollerenceInMs);
+
+  sw.startTimer();
+  await sleep(30);
+  sw.stopTimer();
+
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
+
+  await sleep(1000);
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
+  sw.startTimer();
+  await sleep(1000);
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 2000, tollerenceInMs);
+  await sleep(2000);
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 4000, tollerenceInMs);
+  sw.stopTimer();
+  await sleep(1000);
+  sw.stopTimer();
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 4000, tollerenceInMs);
+  sw.startTimer();
+  await sleep(1000);
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 5000, tollerenceInMs);
+  sw.reset();
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 0, tollerenceInMs);
+  sw.startTimer();
+  await sleep(1000);
+  testTimerDisplaysCorrectTimeAfterMsElapsed(sw, 1000, tollerenceInMs);
+});
+
+function testTimerDisplaysCorrectTimeAfterMsElapsed(
+  stopWatchObject,
+  ms,
+  msTollerence
+) {
+  const re = new RegExp(`00:0${Math.floor(ms / 1000)}:0[0-9]`);
+  expect(stopWatchObject.getTimeElapsedInMs).toBeGreaterThanOrEqual(ms);
+  expect(stopWatchObject.getTimeElapsedInMs).toBeLessThanOrEqual(
+    ms + msTollerence
+  );
+  expect(stopWatchObject.getTimeElapsedInStopWatchFormatString).toMatch(re);
 }
-
--- dist/StopWatch.js
+++ dist/StopWatch.js
@@ -1,64 +1,75 @@
-class StopWatch{
-    constructor() {
-        this.prevTimeElapsed = 0;
-        this.start = null;
-        this.resume = false;
+class StopWatch {
+  constructor() {
+    this.prevTimeElapsed = 0;
+    this.start = null;
+    this.resume = false;
+  }
+
+  startTimer() {
+    if (this.start == null || this.resume) {
+      this.start = Date.now();
+      this.resume = false;
     }
+  }
 
-    startTimer(){
-        if(this.start == null || this.resume){
-            this.start = Date.now();
-            this.resume = false; 
-        }
+  stopTimer() {
+    if (this.start && this.resume == false) {
+      this.resume = true;
+      this.prevTimeElapsed = this.prevTimeElapsed + (Date.now() - this.start);
     }
+  }
 
-    stopTimer(){
-        if(this.resume == false){
-            this.resume = true; 
-            this.prevTimeElapsed = this.prevTimeElapsed + (Date.now() - this.start);
-        }
+  reset() {
+    this.start = null;
+    this.prevTimeElapsed = 0;
+  }
+
+  get getTimeElapsedInMs() {
+    if (this.start == null) {
+      // if stopwatch hasn't started yet or is reset
+      return 0;
+    } else if (this.resume) {
+      // if stop watch is currently stopped
+      return this.prevTimeElapsed;
+    } else {
+      // get time whilst stopwatch is not stopped
+      return this.prevTimeElapsed + (Date.now() - this.start);
     }
+  }
 
-    reset(){
-        this.start = null;
-        this.prevTimeElapsed = 0;
-    }
+  get getSecondsElapsedIn60Seconds() {
+    return Math.floor((this.getTimeElapsedInMs / 1000) % 60);
+  }
 
-    get getTimeElapsedInMs(){
-        if(this.start == null){ // if stopwatch hasn't started yet or is reset
-            return 0;
-        }else if(this.resume){ // if stop watch is currently stopped
-            return this.prevTimeElapsed;
-        }else{ // get time whilst stopwatch is not stopped
-            return this.prevTimeElapsed + (Date.now() - this.start);
-        }
-    }
+  get getTimeElapsedInMinutes() {
+    return Math.floor(this.getTimeElapsedInMs / 1000 / 60);
+  }
 
-    get getSecondsElapsedIn60Seconds(){
-        return Math.floor(this.getTimeElapsedInMs / 1000 % 60);
-    }
+  get getCurrentMillaSecondElapsedAsFraction() {
+    return (this.getTimeElapsedInMs % 1000) / 1000;
+  }
 
-    get getTimeElapsedInMinutes(){
-        return Math.floor(this.getTimeElapsedInMs / 1000 / 60);
-    }
+  get getTimeElapsedInStopWatchFormatString() {
+    // console.log("mins");
+    // console.log(this.getTimeElapsedInMinutes);
+    let minsNumberAsString = String(this.getTimeElapsedInMinutes);
+    let minsAs2Digits =
+      minsNumberAsString.length > 1
+        ? minsNumberAsString
+        : "0" + minsNumberAsString;
 
-    get getCurrentMillaSecondElapsedAsFraction(){
-        return this.getTimeElapsedInMs % 1000 / 1000;
-    }
+    let secondsNumberAsString = String(this.getSecondsElapsedIn60Seconds);
+    let secondsAs2Digits =
+      secondsNumberAsString.length > 1
+        ? secondsNumberAsString
+        : "0" + secondsNumberAsString;
 
-    get getTimeElapsedInStopWatchFormatString(){
-        // console.log("mins");
-        // console.log(this.getTimeElapsedInMinutes);
-        let minsNumberAsString = String(this.getTimeElapsedInMinutes);
-        let minsAs2Digits = minsNumberAsString.length > 1 ? minsNumberAsString : "0" + minsNumberAsString;
+    let millaSecondAs2DigitNumberAsString =
+      "0" +
+      String(Math.floor(this.getCurrentMillaSecondElapsedAsFraction * 10));
 
-        let secondsNumberAsString = String(this.getSecondsElapsedIn60Seconds);
-        let secondsAs2Digits = secondsNumberAsString.length > 1 ? secondsNumberAsString : "0" + secondsNumberAsString;
-
-        let millaSecondAs2DigitNumberAsString = "0" + String(Math.floor(this.getCurrentMillaSecondElapsedAsFraction * 10));
-
-        return `${minsAs2Digits}:${secondsAs2Digits}:${millaSecondAs2DigitNumberAsString}`;
-    }
+    return `${minsAs2Digits}:${secondsAs2Digits}:${millaSecondAs2DigitNumberAsString}`;
+  }
 }
 
 module.exports = StopWatch;
